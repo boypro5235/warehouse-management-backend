c75a9f82c05d16e20758858d07cd84a92753964d
minhlong1802 <137919484+minhlong1802@users.noreply.github.com>
2025-02-01 16:17:10 +0700

    BE project base

---
+++ src/main/java/com/example/WebQlyKho/WebQlyKhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebQlyKho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQlyKhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQlyKhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebQlyKho/WebQlyKhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebQlyKho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQlyKhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
